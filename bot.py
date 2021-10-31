47f8b99d084ad513f4c142c9477920b6efc2fe3c
Teky1 <officialteky@gmail.com>
2021-10-31 03:59:31 -0400

    added np, dc, and queue

--- bot.py
+++ bot.py
@@ -6,6 +6,20 @@ from discord import FFmpegPCMAudio
 
 client = commands.Bot(command_prefix="-")
 server_queues = {}
+FFMPEG_OPTIONS = {
+    'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5',
+    'options': '-vn'
+}
+ydl_opts = {
+    'format': 'bestaudio/best',
+    'postprocessors': [
+        {
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+        }
+    ],
+}
 
 
 class ServerQueue:
@@ -15,55 +29,69 @@ class ServerQueue:
         self.current_queue_number = None
         self.queue = []
         self.channel = None
+        self.active_text_channel = None  # the text channel that the bot was most recently talked to in
         self.looping = False
 
+    def current_song(self):
+        return self.queue[self.current_queue_number]
 
 class Song:
-    def __init__(self, *, url=None, query=None, ):
+    def __init__(self, *, url=None):
         self.url = url
-        self.title = None
-        self.length = None
+
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            info_dict = ydl.extract_info(self.url, download=False)
+
+        self.info_dict = info_dict
+        self.title = self.info_dict.get("title", "No Title Found")
+        self.length = self.info_dict.get("duration", 0)
+        self.length_formatted = f"{round(self.length / 60)}:{str(self.length % 60).zfill(2)}"
+        if len(self.title) <= 38:
+            title_formatted = self.title + " "*(38-len(self.title))
+        else:
+            title_formatted = self.title[:35]+"..."
+        self.queue_string = f"{title_formatted} {self.length_formatted}"
+
 
 
 @client.event
 async def on_ready():
     print("Ready.")
 
-@client.command(name="test")
-async def test(ctx: commands.Context):
-    await ctx.reply("Testing, testing, 123")
+
+@client.event
+async def on_voice_state_update(member: discord.Member, before: discord.VoiceState, after: discord.VoiceState):
+    if client.user.id == member.id and not before.channel is None and after.channel is None:
+        sq = server_queues[before.channel.guild.id]
+        sq.is_playing = False
+        sq.current_queue_number = None
+        sq.queue.clear()
+        try:
+            await sq.server.voice_client.disconnect(force=True)
+        except AttributeError:
+            pass
+        sq.channel = None
+        await sq.active_text_channel.send("Leaving VC and clearing queue...")
 
 def play_song(sq, channel):
     sq.is_playing = True
-    FFMPEG_OPTIONS = {
-        'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5',
-        'options': '-vn'
-    }
-    ydl_opts = {
-        'format': 'bestaudio/best',
-        'postprocessors': [{
-            'key': 'FFmpegExtractAudio',
-            'preferredcodec': 'mp3',
-            'preferredquality': '192',
-        }],
-    }
     voice = get(client.voice_clients, channel=channel)
-    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        info = ydl.extract_info(sq.queue[sq.current_queue_number].url, download=False)
-        voice.play(FFmpegPCMAudio(info["url"], **FFMPEG_OPTIONS),
-                   after=lambda x: on_song_end(sq, channel))
+    voice.play(FFmpegPCMAudio(sq.current_song().info_dict["url"], **FFMPEG_OPTIONS),
+               after=lambda x: on_song_end(sq, channel))
+
 
 def on_song_end(sq, channel):
-    if sq.current_queue_number < len(sq.queue)-1:
+    if sq.current_queue_number is None:
+        return
+    if sq.current_queue_number < len(sq.queue) - 1:
         sq.current_queue_number += 1
-    elif not sq.looping and sq.current_queue_number == len(sq.queue)-1:
+    elif not sq.looping and sq.current_queue_number == len(sq.queue) - 1:
         sq.is_playing = False
         return
-    elif sq.looping and sq.current_queue_number == len(sq.queue)-1:
+    elif sq.looping and sq.current_queue_number == len(sq.queue) - 1:
         sq.current_queue_number = 0
 
     play_song(sq, channel)
-
 
 
 @client.command(name="play", aliases=["p"])
@@ -72,7 +100,7 @@ async def play(ctx: commands.Context, *, query: str):
     if ctx.guild.id not in server_queues:
         server_queues[ctx.guild.id] = ServerQueue(ctx.guild)
     sq = server_queues[ctx.guild.id]
-
+    sq.active_text_channel = ctx.channel
     channel = ctx.author.voice.channel
 
     sq.queue.append(song)
@@ -81,23 +109,34 @@ async def play(ctx: commands.Context, *, query: str):
         await channel.connect()
         sq.channel = channel
 
+    await ctx.send(f"Added {song.title} ({song.length_formatted})")
 
-    if sq.is_playing:
-        await ctx.reply("added song")
-    else:
-        sq.current_queue_number = len(sq.queue)-1
+    if not sq.is_playing:
+        sq.current_queue_number = len(sq.queue) - 1
         play_song(sq, channel)
 
 
+@client.command(name="disconnect", aliases=["dc"])
+async def disconnect(ctx: commands.Context):
+    await ctx.voice_client.disconnect()
 
 
+@client.command(name="queue", aliases=["q"])
+async def queue(ctx: commands.Context):
+    message = "```\n"
+    sq = server_queues[ctx.guild.id]
+    for i,song in enumerate(sq.queue):
+        prefix = " >>> " if sq.current_queue_number == i else "     "
+        message += f"{prefix} {i+1}) {sq.queue[i].queue_string}\n"
+    message += "```"
+    await ctx.send(message)
 
 
-
-
-
-
-
+@client.command(name="nowplaying", aliases=["np"])
+async def nowplaying(ctx: commands.Context):
+    sq = server_queues[ctx.guild.id]
+    song = sq.current_song()
+    await ctx.send(f"Now Playing: `{song.title}`\n({song.url})")
 
 
 
