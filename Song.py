5afd55912bf389df8cddf18ae74dc1316eed608a
Teky1 <officialteky@gmail.com>
2021-11-27 00:56:04 -0500

    hopefully fixed forbidden thing

--- Song.py
+++ Song.py
@@ -1,4 +1,6 @@
+import asyncio
 import youtube_dl
+from urllib.error import HTTPError
 
 ydl_opts = {
     'format': 'bestaudio/best',
@@ -15,8 +17,18 @@ class Song:
     def __init__(self, *, url=None):
         self.url = url
 
-        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-            info_dict = ydl.extract_info(self.url, download=False)
+        request_success = False
+
+        retries = 0
+        while not request_success and retries < 5:
+            try:
+                with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+                        info_dict = ydl.extract_info(self.url, download=False)
+                request_success = True
+            except HTTPError:
+                retries += 1
+                for x in range(10):
+                    print("YOU WERE RIGHT ABOUT THE HTTP FORBIDDEN 403 ERROR THING")
 
         self.info_dict = info_dict
         self.title = self.info_dict.get("title", "No Title Found")
