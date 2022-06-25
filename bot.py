606c7227bff1e0909c666b1fad738b3355e50473
Teky1 <officialteky@gmail.com>
2022-06-25 01:06:39 -0400

    embed

--- bot.py
+++ bot.py
@@ -436,10 +436,12 @@ async def playlist(ctx: commands.Context, playlistName: typing.Optional[str]):
 @client.command(name="playlists", aliases=["listplaylists", "listpl"])
 async def playlists(ctx: commands.Context):
     data = db.getAllData()
-    message = "**All Playlists**\n"
+    # message = "**All Playlists**\n"
+    embed = discord.Embed(title="All Playlists", color=0x595959)
     for pl in data:
-        message += f"{pl[0]} - {pl[2].count(',')+1} songs\n"
-    await ctx.send(message)
+        # message += f"{pl[0]} - {pl[2].count(',')+1} songs\n"
+        embed.add_field(name=pl[0], value=f"{pl[2].count(',')+1} songs", inline=True)
+    await ctx.send(embed=embed)
 
 @commands.cooldown(rate=1, per=1, type=BucketType.member)
 @client.command(name="spotify")
