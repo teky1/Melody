fc248bc7ae72154acf90a5ea351ff09e34735729
Teky1 <officialteky@gmail.com>
2023-04-05 19:53:27 -0400

    intents 2

--- bot.py
+++ bot.py
@@ -19,7 +19,7 @@ import typing
 import spotipy
 import ytmusicapi
 
-client = commands.Bot(command_prefix="-", intents=discord.Intents.default())
+client = commands.Bot(command_prefix="-", intents=discord.Intents.all())
 server_queues = {}
 FFMPEG_OPTIONS = {
     'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5',
